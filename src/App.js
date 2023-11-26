70e5997fd2d12f730f631c0cccd59cb7e6c95940
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-26 15:07:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
-
-// Components
 import Navigation from './components/Navigation'
 import Section from './components/Section'
 import Product from './components/Product'
-
-// ABIs
 import Dappazon from './abis/Dappazon.json'
-
-// Config
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+    console.log(account);
+  }
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
-
-      <h2>Welcome to Pharma Sync</h2>
-
+      <Navigation account={account} setAccount={setAccount} />
+      <h2>Welcome to PharmaSync</h2>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
