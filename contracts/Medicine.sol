fcb2334d7beb281252c98c2c5ad4c87b346a5df1
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-26 13:52:33 +0530

    sm

--- contracts/Medicine.sol
+++ contracts/Medicine.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.9;
 
 contract Medicine {
     // string public name;
@@ -16,24 +16,40 @@ contract Medicine {
         uint256 stock;
     }
 
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
     mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+    modifier onlyOwner () {
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor() {
         // name = "Pharma Nexus";
         owner = msg.sender;
     }
 
-    //List Products
-    function list(uint256 _id, 
-        string memory _name, 
+    //list Products
+    function list(
+        uint256 _id,
+        string memory _name,
         string memory _category,
         string memory _image,
         uint256 _cost,
         uint256 _rating,
         uint256 _stock
-    ) 
-        public {
-            //item struct
+    ) public onlyOwner {
+        // require(msg.sender == owner);
+
+        //item struct
         Item memory item = Item({
             id: _id,
             name: _name,
@@ -45,12 +61,34 @@ contract Medicine {
         });
 
         //save item struct to blockchain
+        items[_id] = item;
 
-    } 
+        //emit an event
+        emit List(_name, _cost, _stock);
 
-    //Buy products
+    }
+
+    //buy medicine
+    function buy(uint256 _id) public payable{
+        //fetch item
+        Item memory item = items[_id];
+
+        //create an order
+        Order memory order = Order(
+            block.timestamp,
+            item
+        );
+
+        //save order to chain
 
 
-    //Withdraw funds
+        //substrack stock
 
+
+        //emit an event
+    }
+
+    //withdraw funds
 }
+
+
--- test/Medicine.js
+++ test/Medicine.js
@@ -1,17 +1,28 @@
 
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+//global const for listing an item
+const ID = 1;
+const NAME = "Paracetamol";
+const CATEGORY = "Antibiotics";
+const IMAGE = "https://www.orionlifes.com/wp-content/uploads/2022/04/brexodol-tab-1024x768.jpeg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
+
+
 describe("Medicine", () => {
   let medicine;
   let deployer, buyer;
 
-  beforeEach(async() => {
+  beforeEach(async () => {
     //setup aaccounts
-    [deployer,buyer] = await ethers.getSigners();
+    [deployer, buyer] = await ethers.getSigners();
     // console.log(deployer.address, buyer.address)
 
     //deploy contract
@@ -23,7 +34,7 @@ describe("Medicine", () => {
 
 
   describe("Deployment", () => {
-    it("Sets the owner", async() => {
+    it("Sets the owner", async () => {
       expect(await medicine.owner()).to.equal(deployer.address)
     })
 
@@ -31,5 +42,70 @@ describe("Medicine", () => {
     //   expect(await medicine.name()).to.equal('Pharma Nexus')
     // })
   })
- 
+
+  describe("Listing", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      transaction = await medicine.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+      await transaction.wait();
+    })
+
+    it("Returns item attributes", async () => {
+      const item = await medicine.items(ID)
+      expect(item.id).to.equal(ID)
+      expect(item.name).to.equal(NAME)
+      expect(item.category).to.equal(CATEGORY)
+      expect(item.image).to.equal(IMAGE)
+      expect(item.cost).to.equal(COST)
+      expect(item.rating).to.equal(RATING)
+      expect(item.stock).to.equal(STOCK)
+    })
+    it("Emits list event", () => {
+      expect(transaction).to.emit(medicine, 'list')
+    })
+  })
+
+  describe("Listing", () => {
+    let transaction;
+
+    //list an item
+    beforeEach(async () => {
+      transaction = await medicine.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+      await transaction.wait();
+
+      //buy an item
+      transaction = await medicine.connect(buyer).buy(ID, { value: COST });
+
+    })
+
+
+    it("Update the contract balance", async () => {
+      const result = await ethers.provider.getBalance(medicine.address)
+      console.log(result)
+      expect(result).to.equal(COST)
+    })
+
+  })
+
 })
+
+
+
+
